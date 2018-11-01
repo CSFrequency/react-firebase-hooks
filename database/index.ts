777db1f8e5ca5709fd10aa9fc1fc02048ba4debf
Chris Bianca <me@chrisbianca.com>
2018-11-01 16:23:08 +0000

    [database] Add useObjectVal hook

--- database/index.ts
+++ database/index.ts
@@ -1,2 +1,3 @@
 export { default as useList, ListHook } from './useList';
 export { default as useObject, ObjectHook } from './useObject';
+export { default as useObjectVal, ObjectValHook } from './useObjectVal';
---
+++ database/useObjectVal.ts
@@ -0,0 +1,17 @@
+import { database } from 'firebase';
+import useObject from './useObject';
+
+export type ObjectValHook<T> = {
+  error?: object;
+  loading: boolean;
+  value?: T;
+};
+
+export default <T>(query: database.Query): ObjectValHook<T> => {
+  const { error, loading, value } = useObject(query);
+  return {
+    error,
+    loading,
+    value: value ? value.val() : undefined,
+  };
+};
