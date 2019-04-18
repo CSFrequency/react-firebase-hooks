a72b6f4d90cf626ea9309b3bd46d928f9a8a98eb
Chris Bianca <me@chrisbianca.com>
2019-04-18 09:36:48 +0100

    Export new `Once` listeners

--- firestore/index.ts
+++ firestore/index.ts
@@ -1,10 +1,14 @@
 export { default as useCollection, CollectionHook } from './useCollection';
+export { default as useCollectionOnce } from './useCollectionOnce';
 export {
   default as useCollectionData,
   CollectionDataHook,
 } from './useCollectionData';
+export { default as useCollectionDataOnce } from './useCollectionDataOnce';
 export { default as useDocument, DocumentHook } from './useDocument';
+export { default as useDocumentOnce } from './useDocumentOnce';
 export {
   default as useDocumentData,
   DocumentDataHook,
 } from './useDocumentData';
+export { default as useDocumentDataOnce } from './useDocumentDataOnce';
