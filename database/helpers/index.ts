e5131c879f7ddcd45efdf8ace22812b084d7ca6e
Chris Bianca <me@chrisbianca.com>
2019-04-18 08:40:24 +0100

    Detect if snapshot value is an object before adding keyfield

--- database/helpers/index.ts
+++ database/helpers/index.ts
@@ -1,9 +1,16 @@
 import { database } from 'firebase';
+import * as isObject from 'isobject';
 
 export const snapshotToData = (
   snapshot: database.DataSnapshot,
   keyField?: string
-) => ({
-  ...snapshot.val(),
-  ...(keyField ? { [keyField]: snapshot.key } : null),
-});
+) => {
+  const val = snapshot.val();
+  if (isObject(val)) {
+    return {
+      ...val,
+      ...(keyField ? { [keyField]: snapshot.key } : null),
+    };
+  }
+  return val;
+};
