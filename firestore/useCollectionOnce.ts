50d79fee3b38101db3a71e0f11915c7523ef4950
Nick De Frangh <nick.defrangh@gmail.com>
2019-12-23 13:17:09 +0100

    Preserve loading state
    Use reset instead of set value when no value is present

--- firestore/useCollectionOnce.ts
+++ firestore/useCollectionOnce.ts
@@ -18,19 +18,16 @@ export const useCollectionOnce = (
   >();
   const ref = useIsEqualRef(query, reset);
 
-  useEffect(
-    () => {
-      if (!ref.current) {
-        setValue(undefined);
-        return;
-      }
-      ref.current
-        .get(options ? options.getOptions : undefined)
-        .then(setValue)
-        .catch(setError);
-    },
-    [ref.current]
-  );
+  useEffect(() => {
+    if (!ref.current) {
+      reset();
+      return;
+    }
+    ref.current
+      .get(options ? options.getOptions : undefined)
+      .then(setValue)
+      .catch(setError);
+  }, [ref.current]);
 
   return [value, loading, error];
 };
