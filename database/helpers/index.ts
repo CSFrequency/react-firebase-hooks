968144e189c8b79a9e3308fc26d4362ba14cb2b7
James Powell <james@joinwaffle.com>
2021-05-25 21:42:56 +0100

    Updated database functions with compat imports

--- database/helpers/index.ts
+++ database/helpers/index.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 export type ValOptions<T> = {
   keyField?: string;
--- database/types.ts
+++ database/types.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import { LoadingHook } from '../util';
 
 export type Val<
