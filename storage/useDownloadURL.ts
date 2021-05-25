d73847cc1fdd2746926f75f95949bcd5aa539a3d
James Powell <james@joinwaffle.com>
2021-05-25 21:29:48 +0100

    Updated storage functions

--- storage/useDownloadURL.ts
+++ storage/useDownloadURL.ts
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
+import { FirebaseStorageError, StorageReference, getDownloadURL } from 'firebase/storage';
 import { useEffect } from 'react';
 import { LoadingHook, useComparatorRef, useLoadingValue } from '../util';
 
-export type DownloadURLHook = LoadingHook<string, firebase.FirebaseError>;
+export type DownloadURLHook = LoadingHook<string, FirebaseStorageError>;
 
 export default (
-  storageRef?: firebase.storage.Reference | null
+  storageRef?: StorageReference | null
 ): DownloadURLHook => {
   const { error, loading, reset, setError, setValue, value } = useLoadingValue<
     string,
-    firebase.FirebaseError
+    FirebaseStorageError
   >();
   const ref = useComparatorRef(storageRef, isEqual, reset);
 
@@ -18,15 +18,15 @@ export default (
       setValue(undefined);
       return;
     }
-    ref.current.getDownloadURL().then(setValue).catch(setError);
+    getDownloadURL(ref.current).then(setValue).catch(setError);
   }, [ref.current]);
 
   return [value, loading, error];
 };
 
 const isEqual = (
-  v1: firebase.storage.Reference | null | undefined,
-  v2: firebase.storage.Reference | null | undefined
+  v1: StorageReference | null | undefined,
+  v2: StorageReference | null | undefined
 ): boolean => {
   const bothNull: boolean = !v1 && !v2;
   const equal: boolean = !!v1 && !!v2 && v1.fullPath === v2.fullPath;
